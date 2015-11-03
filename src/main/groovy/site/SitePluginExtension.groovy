07b9f3189b9c72079e4890c0536e27c3c6ff21d6
Francesco Komauli <francesco.komauli@gmail.com>
2015-11-03 12:22:38 +0100

    Indentation as a build parameter

--- src/main/groovy/site/SitePluginExtension.groovy
+++ src/main/groovy/site/SitePluginExtension.groovy
@@ -5,4 +5,5 @@ class SitePluginExtension {
     final String srcDir = 'src/main/site'
     final String resourcesDir = 'src/main/resources'
     final String buildDir = 'build/site'
+    int indentation = 4
 }
--- src/main/groovy/site/task/Site.groovy
+++ src/main/groovy/site/task/Site.groovy
@@ -25,13 +25,13 @@ class Site extends DefaultTask {
         def target = project.file("$project.site.buildDir/${pageName}.html")
         target.parentFile.mkdirs()
         def out = new PrintWriter(new FileOutputStream(target))
-        def document = new HtmlBuilder(out)
+        def document = new HtmlBuilder(out, project.site.indentation)
         def shell = new GroovyShell(new Binding([document: document]), configuration())
         shell.evaluate("document.with { ${script.file.text} }")
         out.flush()
         logger.lifecycle " >> /${pageName}.html"
     }
-    
+
     private CompilerConfiguration configuration() {
         def config = new CompilerConfiguration();
         config.classpath << project.sourceSets.main.output.classesDir.toString()
